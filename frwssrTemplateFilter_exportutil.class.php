804abace25fb9814bbff96ef541500305d60c2eb
FEUERWASSER <arno.n@feuerwasser.de>
2024-10-25 12:19:37 +0200

    Add files via upload

---
+++ frwssrTemplateFilter_exportutil.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Perch template filter for escaping quotationmarks
+ * 
+ * @package PerchTemplateFilter_exportutil
+ * @version v1.0.0 
+ * @link https://github.com/frwssr/frwssr_exportutil
+ */
+
+class PerchTemplateFilter_exportutil extends PerchTemplateFilter 
+{
+    public function filterAfterProcessing($value, $valueIsMarkup = false)
+    {	
+        $search  = [ '"',  "\r\n" ];
+        $replace = [ '\"', "" ];
+    	$return = str_replace($search, $replace, $value);
+        return $return;
+    }
+}
+
+PerchSystem::register_template_filter('exportutil', 'PerchTemplateFilter_exportutil');
